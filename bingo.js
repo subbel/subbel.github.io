617230c303ae69d390d02d3a23f5f74aae66d359
subbel <pkumar96@asu.edu>
2026-01-20 18:35:02 -0700

    all bingo

--- bingo.js
+++ bingo.js
@@ -25,7 +25,7 @@
     var box24 = document.getElementById("box24");
     var box25 = document.getElementById("box25");
 
-    var solutions = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25],[1,6,11,16,21],[2,7,12,17,22],[3,8,13,18,23],[4,9,14,199,24],[5,10,15,20,25],[1,7,13,19,25]]
+    var solutions = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14 , 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]]
 
 function addListeners(){
     box1.addEventListener('click',handleChange1,{once:true});
